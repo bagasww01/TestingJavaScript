0d19c0eede40c6cfb20cd4f6eaf08a6b42fc573c
Bagas Wiradinata <bagas.wiradinata01@gmail.com>
2024-02-03 23:26:47 +0800

    Update script.js

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -17,7 +17,7 @@ function isInvalidInput() {
   str.match(regex);
   return str.match(regex);
 }
- 
+
 function addEntry() {
   const targetId = '#' + entryDropdown.value;
   const targetInputContainer = document.querySelector(`${targetId} .input-container`);
